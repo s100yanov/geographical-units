e502c0ea3abe70c1bfafff9162e7a8054b013008
S Stoyanov <svetlioemp@gmail.com>
2025-01-11 12:54:48 +0200

    Moved the GeographicalUnitsApplication.java into the main package

---
+++ src/main/java/org/training/geographical_units/GeographicalUnitsApplication.java
@@ -0,0 +1,13 @@
+package org.training.geographical_units;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeographicalUnitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeographicalUnitsApplication.class, args);
+	}
+
+}
